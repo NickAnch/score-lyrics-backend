a43d0b70ea93b9aaa040f5ddec68554f1f9309b3
a-nikolay-roonyx <nikolay.anchutin@roonyx.team>
2019-01-29 18:41:44 +0300

    Added Vote field

--- app/serializers/song_show_serializer.rb
+++ app/serializers/song_show_serializer.rb
@@ -1,10 +1,14 @@
 class SongShowSerializer < ActiveModel::Serializer
   attributes :id, :singer, :name, :lyrics, :translate,
-             :views, :linkUrl, :rating, :genre_name
+             :views, :linkUrl, :rating, :genre_name, :vote
   belongs_to :author, serializer: UserSerializer
 
   def genre_name
     object.genre.name
+  end
+
+  def vote
+    object.ratings.find_by(user_id: current_user&.id)
   end
 
   def rating
