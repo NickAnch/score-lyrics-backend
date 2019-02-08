d0066376146da3d01cf8379eaa7684b3fc04dffa
a-nikolay-roonyx <nikolay.anchutin@roonyx.team>
2019-02-08 17:14:44 +0300

    Add route api, changed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,17 @@ require 'mina/puma'
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
+set :shared_dirs, fetch(:shared_dirs, []).push(
+  'tmp/pids',
+  'tmp/sockets',
+  'public/uploads'
+)
+
+set :shared_files, fetch(:shared_files, []).push(
+  'config/database.yml',
+  'config/secrets.yml',
+  'config/master.key'
+)
 
 set :domain, '46.254.19.100'
 set :port, '42777'
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: {format: 'json'},
-             controllers: {
-               registrations: 'registrations'
-             }
-  resources :users, only: :show
-  resource :profile, only: %i[show update]
-  resources :genres, only: %i[index]
+  scope :api, defaults: {format: 'json'} do
+    devise_for :users, defaults: {format: 'json'},
+              controllers: {
+                registrations: 'registrations'
+              }
+    resources :users, only: :show
+    resource :profile, only: %i[show update]
+    resources :genres, only: %i[index]
 
-  resources :songs, only: %i[index create show update] do
-    resources :ratings, only: :create
+    resources :songs, only: %i[index create show update] do
+      resources :ratings, only: :create
+    end
   end
 end
